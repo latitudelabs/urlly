fd902dad82440c73f0c8fb66a7f6d2a76cdc6c2f
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-04-15 23:11:51 +0400

    add more test cases for date and deep nested query params

--- test/index.js
+++ test/index.js
@@ -13,6 +13,8 @@ test.beforeEach(() => {
 const { getSearchParams, updateURLParams } = persistUrlParams(goto);
 const url = new URL(baseURL);
 
+const dateToInput = (d) => new Date(d).toISOString()
+
 test('can update url query params', (t) => {
     t.notThrows(() => updateURLParams({ filter: "videos" }, url.searchParams))
     t.is(location.href, baseURL + '?filter=videos')
@@ -21,4 +23,19 @@ test('can update url query params', (t) => {
 test('can fetch query param from url', (t) => {
     t.notThrows(() => updateURLParams({ filter: "videos" }, url.searchParams))
     t.is(getSearchParams("filter", url.searchParams), 'videos')
+})
+
+test('can update and fetch date query param from url', (t) => {
+    const date = dateToInput(new Date())
+
+    t.notThrows(() => updateURLParams({ date: date }, url.searchParams))
+    t.is(getSearchParams("date", url.searchParams), date )
+})
+
+test('can update and fetch a deep nested props query param from url', (t) => {
+    const date = dateToInput(new Date())
+    const payload = { date: date, filter: "videos", sortBy: "date" }
+
+    t.notThrows(() => updateURLParams({ props: payload }, url.searchParams))
+    t.deepEqual(getSearchParams("props", url.searchParams), payload)
 })
